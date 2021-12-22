d500a1c09eff0962c5fe0f98d6355a255581b16b
Antonius Bayu <34539032+AntoniusBayu@users.noreply.github.com>
2021-12-22 14:53:40 +0700

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Vendingmachine/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vendingmachine from './components/VendingMachine.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Vendingmachine
   }
 }
 </script>
+
+<style lang ="scss" src="./App.scss"></style>
 
 <style>
 #app {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import "@/assets/bootstrap-vue"
 
 Vue.config.productionTip = false
 
